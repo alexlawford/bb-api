f7dda7f7862536fa0854acdcc3dbff0138f4cc24
Alexander Browne <alexlawford@icloud.com>
2024-03-08 20:35:24 +0800

    changed ports

--- app.py
+++ app.py
@@ -1,25 +1,14 @@
-from flask import Flask 
-
-# from flask import Flask, jsonify, request
-# from flask_restful import Api, Resource
-
-# app = Flask(__name__)
-# api = Api(app)
-
-# class UppercaseText(Resource):
-
-#     def get(self):
-#         text = request.args.get('text')
-
-#         return jsonify({"text": text.upper()})
-
-# api.add_resource(UppercaseText, "/uppercase")
-
-# # if __name__ == "__main__":
-# #     app.run(debug=True)
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def index():
-    return '<h1>Deployed!</h1><style>body { display: flex; align-items: center; justify-content: center; height: 100vh; }</style>'
+class HelloWorld(Resource):
+    def get(self):
+        return jsonify({"message": "Hello World"})
+
+api.add_resource(HelloWorld, "/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
