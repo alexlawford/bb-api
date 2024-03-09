a255b0d7b883289b6515d5cef15d8217d68c3144
Alexander Browne <alexlawford@icloud.com>
2024-03-09 10:23:04 +0800

    Stable diffusion 'hello world'

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+import requests
+from PIL import Image
+from io import BytesIO
+from diffusers import DiffusionPipeline
+
+def saveBytescale (data):
+    headers = {
+        'Authorization': 'Bearer public_12a1yrrGGApHW4eVGAfq3RnXk9uv',
+        'Content-Type': 'image/png',
+    }
+    return requests.post('https://api.bytescale.com/v2/accounts/12a1yrr/uploads/binary', headers=headers, data=data)
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return jsonify({"message": "Hello Universe!"})
+def generateImage ():
+    pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", use_safetensors=True)
+    pipeline.to("cuda")
+    return pipeline("An image of a squirrel in Picasso style").images[0]
 
-api.add_resource(HelloWorld, "/")
+class Predict(Resource):
+    def get(self):
+        prediction = generateImage()
+        with BytesIO() as image_binary:
+            prediction.save(image_binary, "png")
+            image_binary.seek(0)
+            result = saveBytescale(image_binary)
+        return result.json()
+
+api.add_resource(Predict, "/")
 
 if __name__ == "__main__":
     app.run(debug=True)
